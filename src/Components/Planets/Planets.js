219cbb292167b1db81e72f26d14ae9c956e1f5f2
jennydang <jenny.dang117@gmail.com>
2019-06-03 21:03:24 +0200

    fixing style in form input

--- src/Components/Planets/Planets.js
+++ src/Components/Planets/Planets.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import api from '../api/api'
 import PlanetCard from './PlanetCard'
+import './Planets.css'
 
 const getPlanetsByClimate = (value) => {
     return api.get(`/planets/search?climate=${value}`)
@@ -44,26 +45,22 @@ class Planets extends React.Component {
     render() {
         return (
             <div>
-                {/* <div className="ui container"> */}
-                    <div className="pageHeader">
-                        <h1>Searching planets by climate type</h1>
-                    </div>
-                    {/* <div className="ui form" style={{ textAlign: "center"}}> */}
-                        <form type="submit" onSubmit={this.onInitialSearch} className="ui form" style={{ textAlign: "center"}}>
-                            {/* <div className="field"> */}
-                                <input 
-                                    type="text" 
-                                    ref={node => this.input = node} 
-                                    placeholder="Please enter the climate type you want to search!"
-                                />
-                                <button type="submit" className="ui black button">Search</button>
-                            {/* </div> */}
-                        </form>
-                    {/* </div> */}
-                    <div className="renderCard">
-                        {this.renderPlanets()}
-                    </div>                
-                {/* </div> */}
+                <div className="pageHeader">
+                    <h1>Searching planets by climate type</h1>
+                </div>
+                <div>
+                    <form className="formSubmit center" type="submit" onSubmit={this.onInitialSearch}>
+                        <input 
+                            type="text" 
+                            ref={node => this.input = node} 
+                            placeholder="Please enter the climate type you want to search!"
+                        />
+                        <button type="submit" className="ui black button">Search</button>         
+                    </form>
+                </div>
+                <div className="renderCard">
+                    {this.renderPlanets()}
+                </div>                
             </div>
         )
     }
