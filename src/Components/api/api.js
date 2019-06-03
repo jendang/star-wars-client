78f5a3d2ba37617eb7d006b669553a8082794da5
jennydang <jenny.dang117@gmail.com>
2019-06-03 16:34:09 +0200

    modified api

--- src/Components/api/api.js
+++ src/Components/api/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // link to server/DB
 export default axios.create({
-    baseURL: "https://desolate-shore-53301.herokuapp.com"
+    baseURL: "https://desolate-shore-53301.herokuapp.com/"
 })
